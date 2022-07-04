e25b6936424cd7e64dfe680b47637a32be8e884d
MatiBravo47 <Matibravoneron@gmail.com>
2022-07-04 20:52:28 -0300

    Mejoro el codigo

--- js/create.js
+++ js/create.js
@@ -1,31 +1,30 @@
 function guardar() {
- 
-    let n = document.getElementById("txtNombre").value
-    let p = document.getElementById("txtPrecio").value
-    let s = document.getElementById("txtStock").value
- 
-    let producto = {
-        nombre: n,
-        precio: p,
-        stock: s
-    }
-    let url = "http://localhost:5000/productos"
-    var options = {
-        body: JSON.stringify(producto),
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-       // redirect: 'follow'
-    }
-    fetch(url, options)
-        .then(function () {
-            console.log("creado")
-            alert("Grabado")
- 
-            // Handle response we get from the API
-        })
-        .catch(err => {
-            //this.errored = true
-            alert("Error al grabar" )
-            console.error(err);
-        })
+  let n = document.getElementById("txtGP").value;
+  let p = document.getElementById("txtCircuito").value;
+  let s = document.getElementById("txtFecha").value;
+
+  let producto = {
+    GP: n,
+    circuito: p,
+    fecha: s,
+  };
+  let url = "http://localhost:5000/productos";
+  var options = {
+    body: JSON.stringify(producto),
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    // redirect: 'follow'
+  };
+  fetch(url, options)
+    .then(function () {
+      console.log("creado");
+      alert("Guardado");
+
+      // Handle response we get from the API
+    })
+    .catch((err) => {
+      //this.errored = true
+      alert("Error al guardar");
+      console.error(err);
+    });
 }
--- js/update.js
+++ js/update.js
@@ -1,41 +1,41 @@
-var args = location.search.substr(1).split('&');
+var args = location.search.substr(1).split("&");
 // lee los argumentos pasados a este formulario
-var parts = []
+var parts = [];
 for (let i = 0; i < args.length; ++i) {
-    parts[i] = args[i].split('=');
+  parts[i] = args[i].split("=");
 }
-console.log(args)
-document.getElementById("txtId").value = parts[0][1]
-document.getElementById("txtNombre").value = parts[1][1]
-document.getElementById("txtPrecio").value = parts[2][1]
-document.getElementById("txtStock").value = parts[3][1]
- 
+console.log(args);
+document.getElementById("txtId").value = parts[0][1];
+document.getElementById("txtGP").value = parts[1][1];
+document.getElementById("txtCircuito").value = parts[2][1];
+document.getElementById("txtFecha").value = parts[3][1];
+
 function modificar() {
-    let id = document.getElementById("txtId").value
-    let n = document.getElementById("txtNombre").value
-    let p = document.getElementById("txtPrecio").value
-    let s = document.getElementById("txtStock").value
-    let producto = {
-        nombre: n,
-        precio: p,
-        stock: s
-    }
-    let url = "http://localhost:5000/productos/"+id
-    var options = {
-        body: JSON.stringify(producto),
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        redirect: 'follow'
-    }
-    fetch(url, options)
-        .then(function () {
-            console.log("modificado")
-            alert("Registro modificado")
-            // Handle response we get from the API
-        })
-        .catch(err => {
-            //this.errored = true
-            console.error(err);
-            alert("Error al Modificar")
-        })      
+  let id = document.getElementById("txtId").value;
+  let n = document.getElementById("txtGP").value;
+  let p = document.getElementById("txtCircuito").value;
+  let s = document.getElementById("txtFecha").value;
+  let producto = {
+    GP: n,
+    circuito: p,
+    fecha: s,
+  };
+  let url = "http://localhost:5000/productos/" + id;
+  var options = {
+    body: JSON.stringify(producto),
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    redirect: "follow",
+  };
+  fetch(url, options)
+    .then(function () {
+      console.log("modificado");
+      alert("Registro modificado");
+      // Handle response we get from the API
+    })
+    .catch((err) => {
+      //this.errored = true
+      console.error(err);
+      alert("Error al Modificar");
+    });
 }
