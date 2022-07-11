822d1364a2dee3f6d6745eef1d4bc1576272bbad
MatiBravo47 <Matibravoneron@gmail.com>
2022-07-11 17:12:33 -0300

    intento heroku 2

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ CORS(app)
 
 # configuro la base de datos, con el nombre el usuario y la clave
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Contrasql47!@localhost/flaskmysql'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://kgzgngbzfzlwfq:e358fe62ed1aae54b613720d8f77f4fc6bfa14f8e8fe1b0634e8ecedb0874901@ec2-54-159-22-90.compute-1.amazonaws.com:5432/dfl3ev38h8l8ao'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://lbmaanhbdzspdo:a9c73518a6ecce04736e18b72f1a6f726140e19b837be2f07a567ff69768a3ff@ec2-3-222-74-92.compute-1.amazonaws.com:5432/d21tg4kjntp8tj'
 #                                               usuario:clave@localhost/nombreBaseDatos
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
